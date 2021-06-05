50fbabf31f73e6d4443f9a4a822fce9801be7eff
Lucas-bk38 <85374319+Lucas-bk38@users.noreply.github.com>
2021-06-05 11:19:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 if (config.runsInWidget) {
-    let widget = createWidget(items);
+    let widget = createWidget();
     Script.setWidget(widget);
     Script.complete();
 } else {}
 
 function createWidget() {
     const w = new ListWidget()
-    w.backgroundColor = new Color("#b00a0f");
+    w.backgroundColor = new Color('#b00a0f');
     w.centerAlignContent();
     w.addText('item.title');
 
