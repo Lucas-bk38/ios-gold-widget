96ebc4de6a0b6f3e0befcaaa346c38814014c9f2
Lucas-bk38 <85374319+Lucas-bk38@users.noreply.github.com>
2021-06-05 11:58:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 if (config.runsInWidget) {
-    let widget = createWidget();
+    let widget = await createWidget();
     Script.setWidget(widget);
     Script.complete();
 } else {}
 
-function createWidget() {
+async function createWidget () {
     const w = new ListWidget()
     w.backgroundColor = new Color('#b00a0f');
-    w.addText('item.title');
-    w.centerAlignContent();
+
+    const url = 'https://query1.finance.yahoo.com/v7/finance/spark?symbols=%5EIXIC&range=1d&interval=5m&indicators=close&includeTimestamps=false&includePrePost=false&corsDomain=finance.yahoo.com&.tsrc=finance',
+    req = new Request(url),
+    res = JSON.parse(await req.loadString()),
+    arr = txt.spark.result.map(e => [ e.symbol, e.response.map(f => f.meta.regularMarketPrice) ]),
+    price = arr[0][1][0];
+
+    w.addText(`L'or est à ${ price }$/g`);
 
     return w;
 }
