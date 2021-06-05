fb4643cb979cc23dc6840aa8d7e884a6132e52b0
Lucas-bk38 <85374319+Lucas-bk38@users.noreply.github.com>
2021-06-05 12:07:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,28 @@ if (config.runsInWidget) {
     let widget = await createWidget();
     Script.setWidget(widget);
     Script.complete();
-} else {}
+} else {
+    Safari.open(`data:text/plain,${ await getPrice() }`)
+}
 
-async function createWidget () {
-    const w = new ListWidget()
-    w.backgroundColor = new Color('#b00a0f');
 
+
+async function getPrice () {
     const url = 'https://query1.finance.yahoo.com/v7/finance/spark?symbols=%5EIXIC&range=1d&interval=5m&indicators=close&includeTimestamps=false&includePrePost=false&corsDomain=finance.yahoo.com&.tsrc=finance',
     req = new Request(url),
-    res = JSON.parse(await req.loadString()),
+    txt = JSON.parse(await req.loadString()),
     arr = txt.spark.result.map(e => [ e.symbol, e.response.map(f => f.meta.regularMarketPrice) ]),
     price = arr[0][1][0];
 
-    w.addText(`L'or est à ${ price }$/g`);
+    return `L'or est à ${ price }$/g`;
+}
+
+async function createWidget () {
+    const w = new ListWidget()
+    w.backgroundColor = new Color('#b00a0f'),
+    txt = await getPrice();
+
+    w.addText(txt);
 
     return w;
 }
