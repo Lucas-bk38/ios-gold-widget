df3dafdf3405f81c1d8ce4b071a19cb51f98b834
Lucas-bk38 <85374319+Lucas-bk38@users.noreply.github.com>
2021-06-05 12:14:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-if (config.runsInWidget) {
-    let widget = await createWidget();
-    Script.setWidget(widget);
-} else
-    WebView.loadHTML(`<h1 align="center">${ await getPrice() }</h1>`, null, new Size(0, 100));
-
-Script.complete();
-
-
-
-async function getPrice () {
+const getPrice = async () => {
     const url = 'https://query1.finance.yahoo.com/v7/finance/spark?symbols=%5EIXIC&range=1d&interval=5m&indicators=close&includeTimestamps=false&includePrePost=false&corsDomain=finance.yahoo.com&.tsrc=finance',
     req = new Request(url),
     txt = JSON.parse(await req.loadString()),
     arr = txt.spark.result.map(e => [ e.symbol, e.response.map(f => f.meta.regularMarketPrice) ]),
     price = arr[0][1][0];
 
+    console.log(`L'or est à ${ price }$/g`);
     return `L'or est à ${ price }$/g`;
 }
 
-async function createWidget () {
+const createWidget = async () => {
     const w = new ListWidget()
     w.backgroundColor = new Color('#b00a0f'),
     txt = await getPrice();
@@ -27,3 +18,16 @@ async function createWidget () {
 
     return w;
 }
+
+
+
+
+
+if (config.runsInWidget) {
+    let widget = await createWidget();
+    Script.setWidget(widget);
+} else {
+    WebView.loadHTML(`<h1 align="center">${ await getPrice() }</h1>`, null, new Size(0, 100));
+}
+
+Script.complete();
